a416cf33c253163400daf565778d692d08be3f82
hykeung92 <hykeung92@gmail.com>
2020-05-11 19:17:41 -0700

    Done.

--- app.rb
+++ app.rb
@@ -5,4 +5,9 @@ class App < Sinatra::Base
     get '/' do
         erb :user_input
     end
+
+    post '/piglatinize' do
+        @piglatin = PigLatinizer.new.piglatinize(params[:user_phrase])
+        erb :results
+    end
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,4 +1,21 @@
 class PigLatinizer
 
+
+    def piglatinize(phrase)
+        phrase.split(" ").map{ |word| piglatinize_word(word)}.join(" ")
+    end
+
+    def piglatinize_word(word)
+        
+        if word.downcase.index(/[aeiou]/) == 0
+            word + "way"
+          else
+            vowel_index = word.index(/[aAeEiIoOuU]/)
+            consonants = word.slice(0..vowel_index-1)
+            word_leftover = word.slice(vowel_index..word.length)
+            word_leftover + consonants + "ay"
+          end
+    end
+
     
 end
